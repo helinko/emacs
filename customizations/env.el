7a92d552367e3d8812c8cd5a54552a5a9ceb1a54
Risto Helinko <risto.helinko@gmail.com>
2017-07-03 14:09:27 +0300

    testing from windows

--- customizations/env.el
+++ customizations/env.el
@@ -7,4 +7,27 @@
 ;; increase font size for better readability
 (set-face-attribute 'default nil :height 140)
 
+;; (defun git-bash-windows-setup () (interactive)
+;;   (setq explicit-shell-file-name
+;; 	     )
+;;   (setq shell-file-name explicit-shell-file-name)           
+;;   (setenv "SHELL" shell-file-name)
+;;   (add-to-list 'exec-path "C:/Program Files/Git/"))
+
+;; (if (equal system-type 'windows-nt)
+;;     (git-bash-windows-setup)
+;; )
+
+(defun udf-windows-setup ()
+  (setq explicit-shell-file-name "C:\\Program Files\\Git\\bin\\bash.exe")
+  (setq shell-file-name "bash.exe")
+  (setq explicit-bash.exe-args '("--login" "-i"))
+  (setenv "SHELL" shell-file-name)
+  (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m))
+
+(if (eq system-type 'windows-nt)
+    (udf-windows-setup))
+
+
+
 
