aa97145bcdbbf8913249ca0167f67cb9271f22d4
Risto Helinko <risto.helinko@gmail.com>
2017-07-03 11:40:36 +0300

    no real changes, just testing push

--- init.el
+++ init.el
@@ -135,7 +135,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2))
+ '(coffee-tab-width 2)
+ '(package-selected-packages
+   (quote
+    (smex ido-ubiquitous cider clojure-mode-extra-font-locking clojure-mode paredit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
